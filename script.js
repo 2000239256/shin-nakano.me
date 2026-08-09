541730bd6dd746a0a6a37e84800e8a3dbce2e000
Shinnosuke Nakano <162259376+2000239256@users.noreply.github.com>
2026-08-10 02:59:33 +0900

    Implement mobile menu toggle functionality

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+/* =====================================
+   ELEMENTS
+===================================== */
 
+const menuButton = document.getElementById("menu-button");
+const navigation = document.querySelector("nav");
+
+
+
+/* =====================================
+   MOBILE MENU
+===================================== */
+
+menuButton.addEventListener("click", () => {
+
+    navigation.classList.toggle("menu-open");
+
+});
